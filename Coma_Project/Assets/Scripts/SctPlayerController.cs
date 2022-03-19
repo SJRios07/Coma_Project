221dc00fdb6328fe3a6739640b032adf73700bc4
SJRios07 <santiago.jimenez_rio@uao.edu.co>
2022-03-19 16:46:39 -0500

    Agregado logicas de pedida de vida

--- Coma_Project/Assets/Scripts/SctPlayerController.cs
+++ Coma_Project/Assets/Scripts/SctPlayerController.cs
@@ -21,6 +21,9 @@ public class SctPlayerController : MonoBehaviour
 
     [HideInInspector]
     public Vector3 posIni;
+    public bool dead;
+
+    GUIManager guimanager;
     float deltaTime;
 
     public Animator leerAnimator;
@@ -34,6 +37,8 @@ public class SctPlayerController : MonoBehaviour
         posIni = transform.position;
         speedPlayer = 0;
         hasKey = false;
+        dead = false;
+        guimanager = FindObjectOfType<GUIManager>();
     }
 
     // Update is called once per frame
@@ -43,34 +48,46 @@ public class SctPlayerController : MonoBehaviour
         speedPlayer = (canJump) ? groundSpeed:airSpeed;
         deltaTime = Time.deltaTime * 500;
 
-        if (Input.GetKey(KeyCode.D))
+        if (!dead)
         {
-            playerRB.AddForce(transform.right * speedPlayer * deltaTime, ForceMode.Force);
-            playerGeo.transform.eulerAngles = new Vector3(0, 90, 0);
+            if (Input.GetKey(KeyCode.D))
+            {
+                playerRB.AddForce(transform.right * speedPlayer * deltaTime, ForceMode.Force);
+                playerGeo.transform.eulerAngles = new Vector3(0, 90, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                playerRB.AddForce(-transform.right * speedPlayer * deltaTime, ForceMode.Force);
+                playerGeo.transform.eulerAngles = new Vector3(0, 270, 0);
+            }
+            if (Input.GetKey(KeyCode.S) && !canJump)
+            {
+                playerRB.AddForce(-transform.up * speedPlayer * deltaTime, ForceMode.Force);
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && canJump)
+            {
+                playerRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+                canJump = false;
+            }
+            else if (Input.GetKeyDown(KeyCode.Space) && doubleJump)
+            {
+                playerRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+                doubleJump = false;
+            }
         }
-        if (Input.GetKey(KeyCode.A))
+        else
         {
-            playerRB.AddForce(-transform.right * speedPlayer * deltaTime, ForceMode.Force);
-            playerGeo.transform.eulerAngles = new Vector3(0, 270, 0);
-        }
-        if (Input.GetKey(KeyCode.S) && !canJump)
-        {
-            playerRB.AddForce(-transform.up * speedPlayer * deltaTime, ForceMode.Force);
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)
-        {
-            playerRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-            canJump = false;
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) && doubleJump)
-        {
-            playerRB.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-            doubleJump = false;
+            playerRB.constraints = RigidbodyConstraints.FreezeAll;
         }
         if (Input.GetKey(KeyCode.Q))
         {
             transform.position = posIni;
             playerRB.AddForce(Vector3.zero, ForceMode.VelocityChange);
+            guimanager.AddLife(100);
+            playerRB.constraints = RigidbodyConstraints.FreezeAll;
+            playerRB.constraints -= RigidbodyConstraints.FreezePositionX;
+            playerRB.constraints -= RigidbodyConstraints.FreezePositionY;
+            dead = false;
         }
 
         //The step size is equal to speed times frame time.
