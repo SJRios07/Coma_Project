b155e979f03f07b7e9d618f6c198db2b0be5875f
gwendyrv <100473240+gwendyrv@users.noreply.github.com>
2022-03-19 10:37:15 -0500

    Botiquines con script cada uno y conectados al hud
    Los botiquines agregan vida al hud

--- Coma_Project/Assets/Scripts/Gwendy scripts/GBotiquin.cs
+++ Coma_Project/Assets/Scripts/Gwendy scripts/GBotiquin.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GBotiquin : MonoBehaviour
 {
+    public int healthAmount;
+    public SctPlayerController player;
+
+    public GUIManager manager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        manager = GameObject.FindObjectOfType<GUIManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
+    private void OnTriggerEnter(Collider other)          
+    {
+        if (other.gameObject.GetComponent<SctPlayerController>())
+        {
+            manager.AddLife(healthAmount);
+            this.gameObject.SetActive(false);
+        }
+            
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<SctPlayerController>() == player)
+        {
+        }
+     
+    }
+
 }
+
+
+
+
+
+
+  
